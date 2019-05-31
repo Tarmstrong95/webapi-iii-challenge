7861a2c4c4e0e6866303ea078561fe9cc77e020d
Tarmstrong95 <triston95strong@gmail.com>
2019-05-31 01:42:29 -0400

    Complete mvp

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,61 @@
-const express = 'express';
-
+const express = require('express');
+const db = require('./postDb')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(posts => {
+        res.send(posts)
+    })
+    .catch(() => {
+        res.send('error')
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id',validatePostId, (req, res) => {
+db.getById(req.params.id)
+.then(post => {
+    res.send(post)
+})
+.catch(() => {
+    res.send('error')
+})
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id',validatePostId, (req, res) => {
+db.remove(req.params.id)
+.then(item => {
+    res.send(item)
+})
+.catch(() => {
+    res.send('error')
+})
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id',validatePostId, (req, res) => {
+db.update(req.params.id, req.body)
+.then(post => {
+    res.send(post)
+})
+.catch(() => {
+    res.send('error')
+})
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+db.getById(req.params.id)
+.then((post) => {
+    if(post.id == req.params.id){
+        next();
+    }else{
+        req.status(400).json({message: 'Invalid post ID'})
+    }
+})
+.catch(() => {
+res.send('error')
+})
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = 'express';
-
+const express = require('express');
+const userRoute = require('./users/userRouter')
+const postRoute = require('./posts/postRouter')
 const server = express();
+
+server.use(express.json());
+server.use(logger)
+server.use('/users', userRoute)
+server.use('/posts', postRoute)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +16,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`)
 
+next();
 };
 
 module.exports = server;
